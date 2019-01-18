ecc1a647b99a81b58d9b7e87de1d599df221c5e1
Unknown <egoavara@gmail.com>
2019-01-18 17:54:32 +0900

    #004 upload

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,62 @@
 extern crate regex;
 extern crate hex;
 extern crate num;
-extern crate llvm_sys;
+extern crate llvm_sys as llvm;
 
 mod expr;
+
 use std::fs::File;
 use std::io::prelude::*;
+use std::ptr;
 use nom::types::CompleteStr;
 use expr::util::*;
 use expr::word;
 
-fn main() {
-    let mut f = File::open("example/example_00.koce").expect("file not found");
-    llvm_sys::support::
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
 
-    match expr::syntax::parse(contents.as_ref()) {
-        Ok(ok) => {
-            println!("value : {:?}", ok);
-        }
-        Err(err) => {
-            println!("{:?}", err);
-        }
+
+fn main() {
+//    let mut f = File::open("example/example_00.koce").expect("file not found");
+//    let mut contents = String::new();
+//    f.read_to_string(&mut contents)
+//        .expect("something went wrong reading the file");
+//
+//    match expr::syntax::parse(contents.as_ref()) {
+//        Ok(ok) => {
+//            println!("value : {:?}", ok);
+//        }
+//        Err(err) => {
+//            println!("{:?}", err);
+//        }
+//    }
+    unsafe {
+        // Set up a context, module and builder in that context.
+        let context = llvm::core::LLVMContextCreate();
+        let module = llvm::core::LLVMModuleCreateWithName(b"nop\0".as_ptr() as *const _);
+        let builder = llvm::core::LLVMCreateBuilderInContext(context);
+
+        // Get the type signature for void nop(void);
+        // Then create it in our module.
+        let void = llvm::core::LLVMVoidTypeInContext(context);
+        let function_type = llvm::core::LLVMFunctionType(void, ptr::null_mut(), 0, 0);
+        let function =
+            llvm::core::LLVMAddFunction(module, b"nop\0".as_ptr() as *const _, function_type);
+
+        // Create a basic block in the function and set our builder to generate
+        // code in it.
+        let bb = llvm::core::LLVMAppendBasicBlockInContext(context,
+                                                           function,
+                                                           b"entry\0".as_ptr() as *const _);
+        llvm::core::LLVMPositionBuilderAtEnd(builder, bb);
+
+        // Emit a `ret void` into the function
+        llvm::core::LLVMBuildRetVoid(builder);
+
+        // Dump the module as IR to stdout.
+        llvm::core::LLVMDumpModule(module);
+
+        // Clean up. Values created in the context mostly get cleaned up there.
+        llvm::core::LLVMDisposeBuilder(builder);
+        llvm::core::LLVMDisposeModule(module);
+        llvm::core::LLVMContextDispose(context);
     }
 }
